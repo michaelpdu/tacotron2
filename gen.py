9b1505d82986297f79facf293fb4a39cfc284d7b
dupei <dupei.dp@gpus.arcslab.com>
2019-05-21 10:34:04 +0800

    add log

--- gen.py
+++ gen.py
@@ -1,9 +1,3 @@
-# import matplotlib
-# %matplotlib inline
-# import matplotlib.pylab as plt
-
-# import IPython.display as ipd
-
 import sys
 sys.path.append('waveglow/')
 import numpy as np
@@ -18,6 +12,7 @@ from text import text_to_sequence
 # from denoiser import Denoiser
 
 import librosa
+from waveglow.glow import WaveGlow
 
 def plot_data(data, figsize=(16, 4)):
     fig, axes = plt.subplots(1, len(data), figsize=figsize)
@@ -47,23 +42,16 @@ for k in waveglow.convinv:
 # denoiser = Denoiser(waveglow)
 
 # Prepare text input
-text = "Waveglow is really awesome!"
+text = "I don't know how to speak chinese."
 sequence = np.array(text_to_sequence(text, ['english_cleaners']))[None, :]
 sequence = torch.autograd.Variable(
     torch.from_numpy(sequence)).cuda().long()
 
 # Decode text input and plot results
 mel_outputs, mel_outputs_postnet, _, alignments = model.inference(sequence)
-# plot_data((mel_outputs.float().data.cpu().numpy()[0],
-#            mel_outputs_postnet.float().data.cpu().numpy()[0],
-#            alignments.float().data.cpu().numpy()[0].T))
 
 # Synthesize audio from spectrogram using WaveGlow
 with torch.no_grad():
     audio = waveglow.infer(mel_outputs_postnet, sigma=0.666)
-# ipd.Audio(audio[0].data.cpu().numpy(), rate=hparams.sampling_rate)
+print('save voice ...')
 save_wav(audio[0].data.cpu().numpy(), 'test.wav')
-
-# # (Optional) Remove WaveGlow bias
-# audio_denoised = denoiser(audio, strength=0.01)[:, 0]
-# ipd.Audio(audio_denoised.cpu().numpy(), rate=hparams.sampling_rate)
