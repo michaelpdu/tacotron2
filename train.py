4a361dc0474d8a72614c46f4c511838c381518da
dupei <dupei.dp@gpus.arcslab.com>
2019-05-21 15:05:46 +0800

    add num_workers in config

--- train.py
+++ train.py
@@ -52,7 +52,8 @@ def prepare_dataloaders(hparams):
         train_sampler = None
         shuffle = True
 
-    train_loader = DataLoader(trainset, num_workers=1, shuffle=shuffle,
+    train_loader = DataLoader(trainset, num_workers=hparams.num_workers, 
+                              shuffle=shuffle,
                               sampler=train_sampler,
                               batch_size=hparams.batch_size, pin_memory=False,
                               drop_last=True, collate_fn=collate_fn)
