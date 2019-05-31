0d4153a7b4f0ad5593ec57b2bacc742d8d1c49c0
dupei <dupei@hangzhou.gpu4.aliyun.com>
2019-05-31 16:35:47 +0800

    add b32 config

--- hparams.py
+++ hparams.py
@@ -13,7 +13,7 @@ def create_hparams(hparams_string=None, verbose=False):
         iters_per_checkpoint=1000,
         seed=1234,
         dynamic_loss_scaling=True,
-        fp16_run=False,
+        fp16_run=True,
         distributed_run=False,
         dist_backend="nccl",
         dist_url="tcp://localhost:54321",
@@ -28,8 +28,10 @@ def create_hparams(hparams_string=None, verbose=False):
         # training_files='filelists/ljs_audio_text_train_filelist.txt',
         # validation_files='filelists/ljs_audio_text_val_filelist.txt',
         # text_cleaners=['english_cleaners'],
-        training_files='filelists/dupei_audio_text_train_filelist.txt',
-        validation_files='filelists/dupei_audio_text_val_filelist.txt',
+        # training_files='filelists/dupei_audio_text_train_filelist.txt',
+        # validation_files='filelists/dupei_audio_text_val_filelist.txt',
+        training_files='filelists/b31_audio_text_train_filelist.txt',
+        validation_files='filelists/b31_audio_text_val_filelist.txt',
         text_cleaners=['transliteration_cleaners'],
 
         ################################
@@ -84,7 +86,7 @@ def create_hparams(hparams_string=None, verbose=False):
         learning_rate=1e-3,
         weight_decay=1e-6,
         grad_clip_thresh=1.0,
-        batch_size=4,
+        batch_size=8,
         mask_padding=True  # set model's padded outputs to padded values
     )
 
